4ef87d4ead415f92026f3a560c0ac33db01bb6ed
joonlee <96dalee@gmail.com>
2025-06-18 09:44:46 +0900

    first commit, list setting, foreach

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Dragonfruit");
+        fruits.add("Mango");
+
+        //  for(int i = 0; i < fruits.size(); i++) {
+        //  System.out.println(fruits.get(i));
+        //  }
+
+
+        for(String fruit: fruits) {
+            System.out.println(fruit);
+        }
+
+        Iterator<String> it = fruits.iterator();
+        while (it.hasNext()) {
+            String item = it.next();
+            System.out.println(item);
+        }
+    }
+}
